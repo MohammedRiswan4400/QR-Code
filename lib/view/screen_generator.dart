ca9000b4efecd04ca31694736f3a9a70e9ee7f30
MohammedRiswan4400 <riswanmt4400@gmail.com>
2023-09-23 23:04:16 +0530

    Generator created

---
+++ lib/view/screen_generator.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class ScreenGenerator extends StatefulWidget {
+  const ScreenGenerator({super.key});
+
+  @override
+  State<ScreenGenerator> createState() => _ScreenGeneratorState();
+}
+
+class _ScreenGeneratorState extends State<ScreenGenerator> {
+  String qrData = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CostomeAppBar(
+            title: "Generate Qr Code",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Enter data to Generate QR Code",
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  qrData = value;
+                });
+              },
+              // controller: passwordController,
+              maxLines: 5,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(15)),
+                filled: true,
+                fillColor: const Color.fromARGB(28, 158, 158, 158),
+                hintText: "Enter Data",
+                hintStyle: const TextStyle(
+                  color: Color.fromARGB(177, 158, 158, 158),
+                ),
+              ),
+              // obscureText: true,
+            ),
+          ),
+          qrData.isEmpty
+              ? const SizedBox()
+              : QrImageView(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 200,
+                ),
+        ],
+      ),
+    ));
+  }
+}
+
+class CostomeAppBar extends StatelessWidget {
+  const CostomeAppBar({
+    super.key,
+    required this.title,
+  });
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(30),
+            // topRight: Radius.circular(70),
+          ),
+          color: Color.fromARGB(255, 28, 28, 28),
+        ),
+        height: 100,
+        width: double.infinity,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 55,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                    ),
+                  ),
+                  Text(
+                    title,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
--- lib/view/screen_home.dart
+++ lib/view/screen_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code/view/screen_generator.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
@@ -6,55 +7,74 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         height: double.infinity,
         width: double.infinity,
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    // blurStyle: BlurStyle.normal,
-                    blurRadius: 3,
-                    offset: Offset(
-                      5,
-                      5,
-                    ),
-                    color: Color.fromARGB(255, 0, 0, 0),
+            HomeTitle(),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ScreenGenerator(),
                   ),
-                ],
-                borderRadius: BorderRadius.circular(8),
-                gradient: LinearGradient(
-                  colors: [
-                    // Color.fromARGB(255, 0, 255, 242),
-                    Color.fromARGB(255, 48, 59, 125),
-                    Color.fromARGB(255, 0, 255, 76),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
+                );
+              },
+              child: CostomeQrContainer(
+                text: "Generator",
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  "QR Code",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            CostomeQrContainer(
-              text: "Generator",
             ),
             CostomeQrContainer(
               text: "Scanner",
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeTitle extends StatelessWidget {
+  const HomeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            // blurStyle: BlurStyle.normal,
+            blurRadius: 3,
+            offset: Offset(
+              5,
+              5,
+            ),
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(8),
+        gradient: const LinearGradient(
+          colors: [
+            // Color.fromARGB(255, 0, 255, 242),
+            Color.fromARGB(255, 48, 59, 125),
+            Color.fromARGB(255, 0, 255, 76),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+          "QR Code",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
@@ -90,7 +110,7 @@ class CostomeQrContainer extends StatelessWidget {
               top: 100,
               bottom: 0,
               child: Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   // border: Border.all(
                   //   width: 3,
                   // ),
@@ -100,7 +120,7 @@ class CostomeQrContainer extends StatelessWidget {
                 child: Center(
                   child: Text(
                     text,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 30,
                       color: Color.fromARGB(255, 0, 255, 128),
                       fontWeight: FontWeight.bold,
